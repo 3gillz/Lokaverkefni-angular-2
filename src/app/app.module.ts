80502176489ff98577692d4a0bef2d6b6fc0e9e0
dmrgunnarsson <dmrgunnarsson@gmail.com>
2017-05-27 16:02:00 +0000

    smartadmin.module merged with app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+//SmartAdmin start
+import {
+  ModalModule, ButtonsModule, TooltipModule, BsDropdownModule, ProgressbarModule, AlertModule, TabsModule,
+  AccordionModule, CarouselModule
+} from 'ngx-bootstrap';
+import {PopoverModule} from "ngx-popover";
+import {SmartadminLayoutModule} from './smartadmin/layout';
+import {I18nModule} from "./smartadmin/i18n/i18n.module";
+import {SmartadminWidgetsModule} from "./smartadmin/widgets/smartadmin-widgets.module";
+import {UtilsModule} from "./smartadmin/utils/utils.module";
+import {UserModule} from "./smartadmin/user/user.module";
+import {StatsModule} from "./smartadmin/stats/stats.module";
+import {InlineGraphsModule} from "./smartadmin/graphs/inline/inline-graphs.module";
+import {SmartadminFormsLiteModule} from "./smartadmin/forms/smartadmin-forms-lite.module";
+import {SmartProgressbarModule} from "./smartadmin/ui/smart-progressbar/smart-progressbar.module";
+import { JsonApiService } from './smartadmin/json-api.service';
+import { LayoutService } from './smartadmin/layout/layout.service';
+import { UserService } from './smartadmin/user/user.service';
+import { SoundService } from "./smartadmin/sound/sound.service";
+//SmartAdmin end
 
 import { AppRoutingModule } from './app-routing.module';
-import { SmartAdminModule } from './smartadmin.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,13 +31,31 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    SmartAdminModule,
+    SmartadminLayoutModule,
+    I18nModule,
+    SmartadminWidgetsModule,
+    UtilsModule,
+    UserModule,
+    StatsModule,
+    InlineGraphsModule,
+    SmartadminFormsLiteModule,
+    SmartProgressbarModule,
+    TooltipModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    ProgressbarModule.forRoot(),
+    AlertModule.forRoot(),
+    TabsModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    JsonApiService,
+    LayoutService,
+    UserService,
+    SoundService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
