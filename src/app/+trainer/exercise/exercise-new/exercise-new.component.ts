0d86036ff2a7e6dce47551fa8885c0b9655929d9
dmrgunnarsson <dmrgunnarsson@gmail.com>
2017-06-03 16:59:38 +0000

    started with new-exercise

--- src/app/+trainer/exercise/exercise-new/exercise-new.component.ts
+++ src/app/+trainer/exercise/exercise-new/exercise-new.component.ts
@@ -11,5 +11,34 @@ export class ExerciseNewComponent implements OnInit {
 
   ngOnInit() {
   }
+  submitExercise(exerciseform, nafn){
+    console.log(exerciseform.name);    
+    console.log(exerciseform.name.attr);
 
+  }
+  public exerciseOptions:any = {
+    rules: {
+      name: {
+        required: true
+      },
+      link: {
+        url: true
+      },
+      description: {
+        required: true,
+        minlength: 10
+      }
+    },
+    messages: {
+      name: {
+        required: 'Please enter name'
+      },
+      link: {
+        url: 'Please enter a VALID url'
+      },
+      description: {
+        required: 'Please enter description'
+      }
+    }
+  };
 }
