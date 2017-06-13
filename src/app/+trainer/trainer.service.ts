11230d77281d692451cad7b9ab3c7180af0a90e9
Ívar Guðmann Jónsson <ivarj.v16@nemandi.ntv.is>
2017-06-13 21:22:00 +0000

    added new customer in trainer

--- src/app/+trainer/trainer.service.ts
+++ src/app/+trainer/trainer.service.ts
@@ -80,4 +80,27 @@ export class TrainerService {
     this.router.navigate([ 'trainer/exercise/list' ]);    
   }
 
+      addNewCustomer(customerForm): Promise<boolean>{
+    let optionalBody = '';
+    for (let x = 1; x < Object.keys(customerForm).length; x++) {
+      let value = (<any>Object).values(customerForm)[x];
+      if (value !== null && value !== "") {
+        optionalBody += `&${Object.keys(customerForm)[x]}=${(<any>Object).values(customerForm)[x]}`;
+      }
+    }
+    let body = `name=${customerForm.name}` + optionalBody;
+    let url = this.apiRoot + "api/Customer/AddCustomer";
+    let token = localStorage.getItem('access_token');
+    let headers = new Headers({ 'Authorization': "Bearer " + token, 'Content-Type': 'application/x-www-form-urlencoded' });
+    let requestOptions = new RequestOptions({ headers: headers });
+    return new Promise((resolve) => {
+      this.http.post(url, body, requestOptions)
+        .map(res => res.json())
+        .subscribe((data) => {
+          resolve(data);
+          data === true ? this.popUpService.updateInfoSuccess("Customer added") : this.popUpService.errorMessage();
+        })
+    });
+    }
+
 }
