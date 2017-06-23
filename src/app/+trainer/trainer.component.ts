554afce1ae72458d55b51b529979575022fcb42b
dmrgunnarsson <dmrgunnarsson@gmail.com>
2017-06-23 13:02:21 +0000

    exercise.service removed from trainer.component

--- src/app/+trainer/trainer.component.ts
+++ src/app/+trainer/trainer.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { TrainerService } from './trainer.service';
-import { ExerciseService } from './exercise.service';
 import { FoodItemService } from './food-item.service';
 
 @Component({
   selector: 'app-trainer',
   templateUrl: './trainer.component.html',
   styleUrls: ['./trainer.component.css'],
-  providers: [ TrainerService, ExerciseService, FoodItemService ]
+  providers: [ TrainerService, FoodItemService ]
 })
 export class TrainerComponent implements OnInit {
 
