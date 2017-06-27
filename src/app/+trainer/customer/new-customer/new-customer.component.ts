39ea547f725a4a174d87c505efc27d5c543a64b9
Ívar Guðmann Jónsson <ivarj.v16@nemandi.ntv.is>
2017-06-27 22:59:25 +0000

    New customer added to new ASP.Net user and added some words in i18n

--- src/app/+trainer/customer/new-customer/new-customer.component.ts
+++ src/app/+trainer/customer/new-customer/new-customer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject, HostListener } from '@angular/core';
+import { Component, OnInit, Input, Inject, HostListener, OnChanges } from '@angular/core';
 import { Http, Headers, Response, RequestOptions } from '@angular/http';
 import { Router } from '@angular/router';
 import { Observable } from "rxjs/Rx";
@@ -6,6 +6,8 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import { Zipcodes } from '../../../models/zipcodes';
 import { TrainerService } from '../../trainer.service';
+import { MiscService } from './../../../services/misc.service';
+import { AccountService } from './../../../services/account.service';
 
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { PopUpService } from "../../../services/popup.service";
@@ -17,7 +19,10 @@ import { PopUpService } from "../../../services/popup.service";
 })
 export class NewCustomerComponent implements OnInit {
 
+    @Input() agreedInput: boolean;
     public customerForm: FormGroup;
+    public submitted: boolean = false;
+    resolved: boolean = true;
     zipcodes : Zipcodes[] = [];
 
   constructor(
@@ -25,52 +30,99 @@ export class NewCustomerComponent implements OnInit {
     private router: Router,
     private http: Http,
     private popUpService: PopUpService,
+    public miscService: MiscService,
+    private accountService: AccountService,
     private trainerService: TrainerService
   ) { 
     this.customerForm = new FormGroup({
       name: new FormControl('', <any>Validators.required),
       email: new FormControl('', <any>Validators.required),
-      kennitala: new FormControl('', <any>Validators.required),
-      phone: new FormControl('', <any>Validators.required),
+      password: new FormControl('', <any>Validators.required),
+      confirmPassword: new FormControl('', [<any>Validators.required, this.validatePasswordConfirmation.bind(this)]),
+      kennitala: new FormControl('', [<any>Validators.required, this.validateKennitalaControl.bind(this), <any>Validators.maxLength(10), <any>Validators.minLength(10)]),
+      phone: new FormControl('', [<any>Validators.maxLength(7), <any>Validators.minLength(7)]),
       gender: new FormControl('', <any>Validators.required),
       jobDifficulty: new FormControl('', <any>Validators.required),
-      height: new FormControl('', <any>Validators.required),
+      height: new FormControl('', [<any>Validators.required, <any>Validators.maxLength(3), <any>Validators.minLength(2)]),
       foodPref: new FormControl('', <any>Validators),
       injury: new FormControl('', <any>Validators),
       allergy: new FormControl('', <any>Validators),
       address: new FormControl('', <any>Validators.required),
+      TRID: new FormControl(''),
       zipcodes_ZIP: new FormControl('', <any>Validators.required),
-      profileImagePath: new FormControl('', <any>Validators),
       country: new FormControl('', <any>Validators)
     });
   }
 
+
+  ngOnChanges(){
+    this.customerForm.patchValue({terms: this.agreedInput})
+  }
+
   ngOnInit() {
-    this.getZipcodes()
+    this.miscService.getZipcodes()
     .then(z => this.zipcodes = z);
   }
 
-  submitCustomer(customerForm){
-    if(customerForm.valid){
-      this.trainerService.addNewCustomer(customerForm.value)
-      .then((resolve)=> {
-        if(resolve === true){
-          this.customerForm.reset();
-        }
-      });
+    validatePasswordConfirmation(control: FormControl): any {
+    if(this.customerForm) {
+      return control.value === this.customerForm.get('password').value ? null : { notSame: true}
     }
-    else{this.popUpService.errorMessage("Sorry, something went wrong");}
   }
 
-  getZipcodes(): Promise<Zipcodes[]>{
-    let url = this.apiRoot + "api/Zipcodes/GetAll";
-    let token = localStorage.getItem('access_token');
-    let headers = new Headers({ 'Authorization': "Bearer " + token, 'Content-Type': 'application/x-www-form-urlencoded' });
-    let requestOptions = new RequestOptions({ headers: headers });
-    return this.http.get(url, requestOptions)
-        .toPromise()
-        .then(response => response.json() as Zipcodes[])
-        .catch(this.handleError)
+    validateKennitalaControl(control: FormControl): any {
+    if(this.customerForm) {
+      return this.miscService.vartoluProfun(control.value) ? null : { notValid: true}
+    }
+  }
+
+    beTrue(control: FormControl): any  {
+    if(this.customerForm) {
+      return control.value == true ? null : { notValid: true};
+    }
+  }
+  nonNegative(control: FormControl): any  {
+    if(this.customerForm) {
+      return control.value < 0 ? { notValid: true} : null;
+    }
+  }
+
+    submitCustomer(customerForm){
+    this.submitted = true;
+    if(customerForm.valid){
+      this.resolved = false;
+      this.accountService.registerUser(customerForm.value, "trainee")
+      .then( data => {
+        if(data[0]){
+          let form = customerForm.value;
+          let trid = JSON.parse(localStorage.getItem('user')).TRID;
+          form.TRID = trid;
+          this.accountService.registerTrainerOrTrainee(customerForm.value, data[1]._body, false)
+          .then( data =>{
+            if(data){
+              this.router.navigate(['login']);
+              this.resolved = true;              
+            }else if(!data){
+              this.resolved = true;              
+            }
+          })
+        }
+        else if(!data[0]){
+          this.resolved = true;
+        }
+      })
+    }
+  }
+
+
+  initialZipcode: any = -1;
+  place: string = "Place";
+  zipCodeChange(zip){
+    for(let x = 0; x < this.zipcodes.length; x++){
+      if(this.zipcodes[x].ZIP == zip){
+        this.place = this.zipcodes[x].place;
+      }
+    }
   }
 
   private handleError(error: any) {
